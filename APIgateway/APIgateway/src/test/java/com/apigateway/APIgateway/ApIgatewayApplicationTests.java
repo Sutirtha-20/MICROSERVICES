caa2e2e293543c3e0e662e4b0262941c71b154fe
Sutirtha <strth1804@gmail.com>
2024-06-01 02:39:43 +0530

    implemented API gateway

---
+++ APIgateway/APIgateway/src/test/java/com/apigateway/APIgateway/ApIgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.APIgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
